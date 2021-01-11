74a1336f9e484131c93e77e8fb610476ff78a8b2
boykj <jboyk918@gmail.com>
2021-01-11 10:59:27 -0800

    Initial commit: Build from class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+//import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHRrFJcgJAIuCWea01pSZhurnGtA6Ps6M",
+  authDomain: "cs493-2021-e2fac.firebaseapp.com",
+  projectId: "cs493-2021-e2fac",
+  storageBucket: "cs493-2021-e2fac.appspot.com",
+  messagingSenderId: "1007924530957",
+  appId: "1:1007924530957:web:7e2012cffac31a476faa44"
+};
+
+// Try to only load the DB when you need it
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+  console.log(firebase);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={async function() {
+          const user = await firebase
+            .auth()
+            .createUserWithEmailAndPassword("testing@test.com", "Test1234"); 
+        alert(JSON.stringify(user))}}>Sign Up</button>
       </header>
     </div>
   );
